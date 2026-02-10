e17c36668a9dcf37f34bbfb8657bf20c604b9f6d
alinarogova123-arch <alinarogova123@gmail.com>
2026-02-10 23:36:22 +0300

    fetch_nasa_apods.py Перенес load_dotenv и try/except в if __name__ == "__main__":

--- fetch_nasa_apods.py
+++ fetch_nasa_apods.py
@@ -5,23 +5,13 @@ from dotenv import load_dotenv
 from load_helpers import load_image
 
 
-def fetch_nasa_apod():
-    load_dotenv(".env")
-    token = os.environ["NASA_API_KEY"]
+def fetch_nasa_apod(token):
     params = {
         'api_key': token,
         'start_date': '2026-01-25',
     }
-    try:
-        response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        params = {
-            'api_key': 'DEMO_KEY',
-            'start_date': '2026-01-25',
-        }
-        response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
-        response.raise_for_status()
+    response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
+    response.raise_for_status()
     i = 0
     for image in response.json():
         load_image(image['url'], f'images/nasa_apod_{i}.jpg')
@@ -29,4 +19,10 @@ def fetch_nasa_apod():
 
 
 if __name__ == "__main__":
-    fetch_nasa_apod()
+    load_dotenv(".env")
+    token = os.environ["NASA_API_KEY"]
+    token_reserv = os.environ["NASA_API_DEMO_KEY"]
+    try:
+        fetch_nasa_apod(token)
+    except requests.exceptions.HTTPError:
+        fetch_nasa_apod(token_reserv)
